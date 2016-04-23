789a2679dac05dc740df60483e4110751015b679
AntoineThebaud <antoine.thebaud@yahoo.fr>
2016-04-24 00:17:20 +0200

    Events redimensionnement OK

--- src/ui/GUI.java
+++ src/ui/GUI.java
@@ -3,10 +3,13 @@ package ui;
 // no compte rendu 
 	
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Rectangle;
 import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
@@ -22,12 +25,12 @@ import javax.swing.table.TableColumn;
 	 
 public class GUI extends JFrame {
 	
-  static int vertSize = 105;
+  static int MVS = 105;
   
   public GUI() {
 	  
     this.setTitle("Multimedia Player");
-    this.setMinimumSize(new Dimension(520, vertSize));
+    this.setMinimumSize(new Dimension(520, MVS));
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLocationRelativeTo(null);
     this.setLayout(new BorderLayout());
@@ -37,8 +40,8 @@ public class GUI extends JFrame {
     //- GAUCHE : Boutons
     JPanel lvl2_LeftPanel = new JPanel();
     lvl2_LeftPanel.setLayout(new FlowLayout());
-    lvl2_LeftPanel.setPreferredSize(new Dimension(180, vertSize));
-    lvl2_LeftPanel.setMinimumSize(new Dimension(180, vertSize));
+    lvl2_LeftPanel.setPreferredSize(new Dimension(180, MVS));
+    lvl2_LeftPanel.setMinimumSize(new Dimension(180, MVS));
     lvl2_LeftPanel.add(new JButton("<"));
     lvl2_LeftPanel.add(new JButton("D"));
     lvl2_LeftPanel.add(new JButton(">"));
@@ -48,7 +51,7 @@ public class GUI extends JFrame {
     
     //- CENTRAL : Slider + Tooltip + Boutons bottom-right
     JPanel lvl2_CentralPanel = new JPanel();
-    lvl2_CentralPanel.setMinimumSize(new Dimension(180, vertSize));
+    lvl2_CentralPanel.setMinimumSize(new Dimension(180, MVS));
     lvl2_CentralPanel.setLayout(new BorderLayout());
     //- slider
     JPanel lvl3_sliderPanel = new JPanel();
@@ -61,7 +64,22 @@ public class GUI extends JFrame {
     JPanel lvl3_rightButtons = new JPanel();
     lvl3_rightButtons.setLayout(new FlowLayout());
     lvl3_rightButtons.add(new JButton("|||"));
-    lvl3_rightButtons.add(new JButton("[_]"));
+    JButton BtShowPlaylist = new JButton("[_]");
+    BtShowPlaylist.addActionListener(new ActionListener() {
+    	Color cliqued = new Color(163, 184, 204);
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			
+			if(BtShowPlaylist.getBackground().equals(cliqued)) {
+				BtShowPlaylist.setBackground(null);
+				setSize(new Dimension(getWidth(), MVS));
+			} else {
+				BtShowPlaylist.setBackground(cliqued);
+				setSize(new Dimension(getWidth(), 350));
+			}
+		}
+    });
+    lvl3_rightButtons.add(BtShowPlaylist);
     
     lvl2_CentralPanel.add(lvl3_text, BorderLayout.NORTH);
     lvl2_CentralPanel.add(lvl3_sliderPanel, BorderLayout.WEST);
@@ -130,31 +148,19 @@ public class GUI extends JFrame {
     this.getContentPane().add(lvl1_Menu, BorderLayout.NORTH);
     this.getContentPane().add(lvl1_Playlist, BorderLayout.CENTER);
     
-    //TODO : à décommenter 
-    //Listener : redimensionnement
-//    this.addComponentListener(new ComponentAdapter() {
-//        @Override
-//        public void componentResized(ComponentEvent e) {
-//            setSize(new Dimension(getWidth(), VHS));
-//        }
-//    });
+//    TODO : à décommenter 
+//    Listener : redimensionnement
+    this.addComponentListener(new ComponentAdapter() {
+        @Override
+        public void componentResized(ComponentEvent e) {
+        	if(getHeight() < 300) {
+        		setSize(new Dimension(getWidth(), MVS));
+        		BtShowPlaylist.setBackground(null);
+        	}
+        }
+    });
 
     this.setVisible(true);
     this.pack();
   }
-  
-  //dans le constructeur :
-  //this.addComponentListener(new GUIListener(this));
-  
-//  public class GUIListener extends ComponentAdapter {
-//	
-//	GUI g;
-//	public GUIListener(GUI g) {
-//		this.g = g;
-//	}
-//	@Override
-//    public void componentResized(ComponentEvent e) {
-//		g.setSize(new Dimension(g.getWidth(), 100));
-//    }
-//  }
 }
